e95d3d0496ab7ea4c54bccb2dcbfdd68ce7460aa
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2022-09-17 00:55:55 -0300

    create global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
