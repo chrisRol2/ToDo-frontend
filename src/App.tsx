efbb53c3be49a00f5caa4cf299999359fb098a8f
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2022-09-27 21:30:05 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
+import Account from "./context/account/account";
+import Dashboard from "./pages/Dashboard";
+// import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <Router>
-      <GlobalStyle />
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Account>
+        <GlobalStyle />
+        <NavBar />
+        <Switch>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Switch>
+      </Account>
     </Router>
   );
 }
